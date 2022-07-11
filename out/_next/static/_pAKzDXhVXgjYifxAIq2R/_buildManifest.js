e9008b576c25d6141c7b0d03f0ea6885c1f5b52f
Siddharth Ray <sidraystudios@gmail.com>
2022-07-10 20:33:06 -0700

    Deploy gh-pages

--- out/_next/static/_pAKzDXhVXgjYifxAIq2R/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/962-b749c12795fa12a8.js","static/css/48e57c1ca4b9fee1.css","static/chunks/pages/index-79aafe3d15c27e84.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
