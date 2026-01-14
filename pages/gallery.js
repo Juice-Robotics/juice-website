59355b46ed7f5ccd4445aabc48cb151121797ec6
awesomehtr1 <awesomehtr1@gmail.com>
2026-01-14 12:47:23 -0800

    gallery update

--- pages/gallery.js
+++ pages/gallery.js
@@ -15,7 +15,7 @@ export default function GalleryRedirect() {
     }, 1000);
 
     useEffect(() => {
-        window.location.assign('https://photos.app.goo.gl/eLMwDNt8TD3G5Mzr9')
+        window.location.assign('https://photos.app.goo.gl/xJHsmLRosoZMGfyLA')
     })
     return (
         <div className={styles.main}>
@@ -23,7 +23,7 @@ export default function GalleryRedirect() {
                 REDIRECTING...
             </div>
             <p style={{opacity: (overtime == true ? 1.0 : 0.0)}}>
-                If it does not redirect, <a href="https://photos.app.goo.gl/eLMwDNt8TD3G5Mzr9" style={{fontWeight: "bold"}}>click here</a></p>
+                If it does not redirect, <a href="https://photos.app.goo.gl/xJHsmLRosoZMGfyLA" style={{fontWeight: "bold"}}>click here</a></p>
         </div>
     )
 }
